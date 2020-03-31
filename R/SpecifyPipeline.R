3467d9b6849a1c1a948338c9763b22963231b218
jeanlucj <jeanluc.work@gmail.com>
2020-03-31 17:58:38 -0400

    Commit to test newFeature

--- R/SpecifyPipeline.R
+++ R/SpecifyPipeline.R
@@ -270,7 +270,8 @@ calcDerivedParms <- function(bsp){
   nChkPlots <- pairwiseComp(nChkPlots, bsp$nReps, max) # At least one check / rep
   chkReps <- ceiling(nChkPlots / bsp$nChks)
   # Safety if nChks or entryToChkRatio misunderstood
-  chkReps <- if_else(chkReps == Inf, 1, chkReps, 1)
+  bsp$nChks <- if_else(bsp$entryToChkRatio == 0, 0, bsp$nChks)
+  chkReps <- if_else(is.infinite(chkReps) | is.nan(chkReps) | is.na(chkReps), 0, chkReps)
   
   # Make sure everything has names
   names(bsp$nEntries) <- names(bsp$nChks) <- names(bsp$nReps) <- names(bsp$nLocs) <- names(bsp$errVars) <- names(chkReps) <- bsp$stageNames
