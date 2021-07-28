fc2a684f35b832800c09467213ed2bee7b5bd83d
Luciano de Andrade <42940692+LucianoRogerio@users.noreply.github.com>
2021-07-28 15:27:05 -0400

    Update AnalysisFunctions.R

--- R/AnalysisFunctions.R
+++ R/AnalysisFunctions.R
@@ -185,26 +185,26 @@ iidPhenoEval <- function(phenoDF){
 
 grmPhenoEval <- function(phenoDF, grm){
 #  if("asreml"%in%ip) {
-    require(asreml)
-    asreml(pheno ~ 1,
-           random = ~ vm(id, grm),
-           residual = ~ units,
-           weights = wgt,
-           data = phenoDF, na.method.X = "omit")
-  blup <- summary(fm, coef = T)$coef.random$solution 
+#    require(asreml)
+#    asreml(pheno ~ 1,
+#           random = ~ vm(id, grm),
+#           residual = ~ units,
+#           weights = wgt,
+#           data = phenoDF, na.method.X = "omit")
+#  blup <- summary(fm, coef = T)$coef.random$solution 
 #  } else {
-#  require(sommer)
-#  phenoDF$id <- factor(phenoDF$id, levels=rownames(grm)) # Enable prediction
-#  phenoDF$wgt <- 1/phenoDF$errVar # Make into weights
-#  fm <- mmer(pheno ~ 1,
-#             random= ~ vs(id, Gu=grm),
-#             method="EMMA",
-#             rcov= ~ units,
-#             weights=wgt,
-#             data=phenoDF,
-#             verbose=F,
-#             date.warning=F)
-#  blup <- fm$U[[1]][[1]]}
+  require(sommer)
+  phenoDF$id <- factor(phenoDF$id, levels=rownames(grm)) # Enable prediction
+  phenoDF$wgt <- 1/phenoDF$errVar # Make into weights
+  fm <- mmer(pheno ~ 1,
+             random= ~ vs(id, Gu=grm),
+             method="EMMA",
+             rcov= ~ units,
+             weights=wgt,
+             data=phenoDF,
+             verbose=F,
+             date.warning=F)
+  blup <- fm$U[[1]][[1]]}
   # Ensure output has variation: needed for optimal contributions
   if (sd(blup) == 0){
     namesBlup <- names(blup)
