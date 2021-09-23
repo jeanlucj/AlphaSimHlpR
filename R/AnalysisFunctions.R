eb23450eea766262accd75d40fa7f40435582eec
Luciano de Andrade <42940692+LucianoRogerio@users.noreply.github.com>
2021-09-22 21:43:16 -0400

    DebugAsremlAdd11

--- R/AnalysisFunctions.R
+++ R/AnalysisFunctions.R
@@ -181,22 +181,18 @@ grmPhenoEval <- function(phenoDF, grm){
 #  if("asreml"%in%installed.packages()) {
     require(asreml)
     print("You decide to use asreml package")
-    saveRDS(grm, "MatrixTest.rds")
-    saveRDS(phenoDF, "PhenoTest.rds")
- #   phenoDF$id <- as.character(phenoDF$id) # Enable prediction
- #   phenoDF$wgt <- 1 / phenoDF$errVar # Make into weights    
- #   attr(grm)$rowNames <- as.character(id)
- #   attr(grm)$colNames <- as.character(id)
- #   attr(grm)$INVERSE <- TRUE
- #   fm <- asreml(fixed=pheno~1,
- #                random=~ped(id),
- #                ginverse=list(id=Ginv),
- #                residual=~units,
- #                weights=wgt,
- #                data=phenoDF,
- #                workspace=128e06,
- #                na.method.X="include")
- #   blup <- summary(fm, coef = T)$coef.random$solution 
+    phenoDF$id <- as.factor(phenoDF$id) # Enable prediction
+    phenoDF$wgt <- 1 / phenoDF$errVar # Make into weights    
+    attr(grm, "rowNames") <- rownames(grm)
+    attr(grm, "colNames") <- rownames(grm)
+    attr(grm, "INVERSE") <- TRUE
+    fm <- asreml(fixed=pheno~1,
+                 random=~vm(id,grm),
+                 residual=~id(units),
+                 weights=wgt,
+                 data=phenoDF,
+                 workspace=128e06)
+    blup <- summary(fm, coef = T)$coef.random$solution 
 #  } else {
 #  require(sommer)
 #  print("You decide to use sommer package")
@@ -213,15 +209,12 @@ grmPhenoEval <- function(phenoDF, grm){
 #  blup <- fm$U[[1]][[1]]
 #}
   # Ensure output has variation: needed for optimal contributions
-#  if (sd(blup) == 0){
-#    namesBlup <- names(blup)
-#    blup <- tapply(phenoDF$pheno, phenoDF$id, mean)
-#    names(blup) <- namesBlup
-#    }
-#  return(blup)
-return(saveRDS(grm, "MatrixTest.rds"), saveRDS(phenoDF, "PhenoTest.rds"))
-       
-
+  if (sd(blup) == 0){
+    namesBlup <- names(blup)
+    blup <- tapply(phenoDF$pheno, phenoDF$id, mean)
+    names(blup) <- namesBlup
+    }
+  return(blup)
 }
 
 #' selCritIID function
