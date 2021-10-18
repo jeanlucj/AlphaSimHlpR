2c1e2f06d752c385fe84dbd5c669b1c788d37ee6
Luciano de Andrade <42940692+LucianoRogerio@users.noreply.github.com>
2021-10-18 14:02:41 -0400

    obtainGmatrixSingProblem

--- R/AnalysisFunctions.R
+++ R/AnalysisFunctions.R
@@ -186,7 +186,7 @@ grmPhenoEval <- function(phenoDF, grm){
     phenoDF$id <- factor(phenoDF$id, levels=rownames(G)) # Enable prediction
     phenoDF$wgt <- 1/phenoDF$errVar # Make into weights
     suppressMessages(Ginv <- G.inverse(G = grm, sparseform = T, blend = T, bend = T)$Ginv)
-    saveRDS(Ginv, "GinvTest.rds")
+    saveRDS(grm, "GTestSing.rds")
             
     fm <- asreml(pheno ~ 1,
                  random = ~ vm(id,Ginv),
